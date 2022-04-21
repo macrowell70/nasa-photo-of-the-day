4ccfa1ea0a41dc986e8272cd9e790e315920f259
unknown <macrowell@yahoo.com>
2022-04-20 21:08:50 -0400

    added search bar for future utility

--- src/Apod.js
+++ src/Apod.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
-
+import Search from './Search'
 
 const Apod = (props) => {
     const { title, hdurl, explanation, copyright, date } = props
+
 
     return(
         <div className='container'>
             <h1>{title} {date}</h1>
             <h2>By: {copyright}</h2>
-            <img src={hdurl} alt='photo of the day' />
+            <img src={hdurl} alt='photo of the day' className='image' />
             <p>{explanation}</p>
+            <Search />
         </div>
     )
 }
--- src/Search.js
+++ src/Search.js
@@ -1,1 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+
+const Search = (props) => {
+  
+
+    return (
+       <form><input type="search" placeholder="YYYY-MM-DD" /></form>
+    )
+
+}
+
+export default Search
