4a1a9801df42f518703529f2aeee2db8b262ec86
unknown <macrowell@yahoo.com>
2022-04-21 18:44:35 -0400

    added basic styles to webpage

--- src/Apod.js
+++ src/Apod.js
@@ -1,18 +1,41 @@
 import React, { useState } from "react";
 import Search from './Search'
+import styledComponents from "styled-components";
+import styled from "styled-components";
+
+
+
+const StyledApod = styled.div`
+    background-color: #483D8B;
+    color: white;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    font-family: sans-serif;
+    text-shadow: 1px 1px 2px black;
+    
+
+
+
+    img {
+        height: 60vh;
+
+    }
+`;
+
 
 const Apod = (props) => {
     const { title, hdurl, explanation, copyright, date } = props
 
 
     return(
-        <div className='container'>
+        <StyledApod className='container'>
             <h1>{title} {date}</h1>
             <h2>By: {copyright}</h2>
             <img src={hdurl} alt='photo of the day' className='image' />
             <p>{explanation}</p>
             <Search />
-        </div>
+        </StyledApod>
     )
 }
 
